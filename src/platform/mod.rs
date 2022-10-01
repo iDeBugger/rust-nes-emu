179ddaa0be28b1c6b71fe2b2ae063caf7aca0c6a
iDeBugger Freeman <me@idebugger.io>
2022-10-01 21:03:45 +0300

    Add RTS test ROM

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -246,4 +246,16 @@ mod test {
         print_rom_result!(mem);
         assert_eq!(mem[0x6000], 0x0);
     }
+
+    #[test]
+    fn test_rom_13_rts() {
+        init();
+
+        let mut platform = Platform::new();
+        platform.load_rom_and_run("./tests/roms/13-rts.nes", true);
+
+        let mem = platform.memory.borrow();
+        print_rom_result!(mem);
+        assert_eq!(mem[0x6000], 0x0);
+    }
 }
